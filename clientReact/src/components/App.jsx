4356719a78236b7140d8b5b7692a03be35c957b9
apopheny <cedouglass@gmail.com>
2024-07-03 14:55:59 -0400

    feat: added sidebar with connections and form links. added rendering of form

--- clientReact/src/components/App.jsx
+++ clientReact/src/components/App.jsx
@@ -3,12 +3,15 @@ import '../App.css';
 import { getConnections } from '../utils/getEntities';
 import { putConnection } from '../utils/putConnections';
 import Modal from './Modal';
+import Sidebar from './Sidebar';
+import AddConnection from './AddConnection';
 
 function App() {
   const [connections, setConnections] = useState([]);
   const [modalDisplayed, setModalDisplayed] = useState(false);
   const [selectedConnection, setSelectedConnection] = useState(null);
   const [connectionAlterations, setConnectionAlterations] = useState(0);
+  const [mainContent, setMainContent] = useState('connections');
 
   useEffect(() => {
     getConnections()
@@ -28,75 +31,81 @@ function App() {
       const result = await putConnection(connectionId, !activeState);
       setConnectionAlterations((prev) => prev + 1);
       return result;
-    }
+    };
 
     request();
   }
 
   function showConfirmPause(connectionId, activeState) {
-    if (window.confirm(`Are you sure you want to pause connection ${connectionId}?`)) {
+    if (
+      window.confirm(
+        `Are you sure you want to pause connection ${connectionId}?`
+      )
+    ) {
       pauseConnection(connectionId, activeState);
     }
   }
 
   return (
     <>
-      <div className='connections-table'>
-        <h1>Connections</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Source Topic</th>
-              <th>Target Topic</th>
-              <th>Transformation Name</th>
-              <th>Pause</th>
-              {/* <th>Delete</th> */}
-            </tr>
-          </thead>
-          <tbody>
-            {connections.map((connection) => (
-              <tr id={connection.id} key={connection.source_topic}>
-                <td>
-                  <a onClick={() => toggleModal(connection)}>
-                    {connection.source_topic}
-                  </a>
-                </td>
-                <td>
-                  <a onClick={() => toggleModal(connection)}>
-                    {connection.target_topic}
-                  </a>
-                </td>
-                <td>
-                  <a onClick={() => toggleModal(connection)}>
-                    {connection.transformation_name}
-                  </a>
-                </td>
-                <td>
-                  <a onClick={() => showConfirmPause(connection.id, connection.active_state)}><img src='../public/pause.png' alt='Pause' /></a>
-                </td>
-                {/* <td>
+      <Sidebar setMainContent={setMainContent} />
+      {mainContent === 'connections' ? (
+        <div className='connections-table'>
+          <h1>Connections</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Source Topic</th>
+                <th>Target Topic</th>
+                <th>Transformation Name</th>
+                <th>Pause</th>
+                {/* <th>Delete</th> */}
+              </tr>
+            </thead>
+            <tbody>
+              {connections.map((connection) => (
+                <tr id={connection.id} key={connection.source_topic}>
+                  <td>
+                    <a onClick={() => toggleModal(connection)}>
+                      {connection.source_topic}
+                    </a>
+                  </td>
+                  <td>
+                    <a onClick={() => toggleModal(connection)}>
+                      {connection.target_topic}
+                    </a>
+                  </td>
+                  <td>
+                    <a onClick={() => toggleModal(connection)}>
+                      {connection.transformation_name}
+                    </a>
+                  </td>
+                  <td>
+                    <a
+                      onClick={() =>
+                        showConfirmPause(connection.id, connection.active_state)
+                      }
+                    >
+                      <img src='../pause.png' alt='Pause' />
+                    </a>
+                  </td>
+                  {/* <td>
                   <img src='../public/bin.png' alt='Delete' />
                 </td> */}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <AddConnection connections={connections} />
+      )}
 
       <Modal
         show={modalDisplayed}
         onClose={() => setModalDisplayed(false)}
         connection={selectedConnection}
-      >
-        {selectedConnection && (
-          <div>
-            <h2>Connection Details</h2>
-            <p>Source Topic: {selectedConnection.source_topic}</p>
-            <p>Target Topic: {selectedConnection.target_topic}</p>
-            <p>Transformation Name: {selectedConnection.transformation_name}</p>
-          </div>
-        )}
-      </Modal>
+      ></Modal>
     </>
   );
 }
